40737bfce8e91998b197c84a4f590128f6741eef
Vanuj447 <vanuj447@gmail.com>
2025-08-19 06:15:57 +0000

    edited home about etc

--- src/components/About.js
+++ src/components/About.js
@@ -41,3 +41,4 @@ function About() {
 }
 
 export default About;
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="profile-wrapper mb-4">
           <img
             src={profileImg}
-            alt="Prasad Reddy"
+            alt="No image"
             className="profile-img shadow"
           />
         </div>
